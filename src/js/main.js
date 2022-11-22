477ad9be6a68ec768eed733eeb5835bd42d5bd09
macdaegeon <chleorjs37@gmail.com>
2022-11-22 21:24:09 +0900

    푸시

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,13 @@
       heightNum: 5, // 브라우저 높이의 5배 scrollHeight 세팅
       objs: {
         container: document.querySelector("#scroll-section-0"),
+        messageA: document.querySelector("#scroll-section-0 .main-message.a"),
+        messageB: document.querySelector("#scroll-section-0 .main-message.b"),
+        messageC: document.querySelector("#scroll-section-0 .main-message.c"),
+        messageD: document.querySelector("#scroll-section-0 .main-message.d"),
+      },
+      values: {
+        messageA_opacity: [0, 1],
       },
     },
     {
@@ -61,6 +68,33 @@
     document.body.setAttribute("id", `show-scene-${currentScene}`);
   }
 
+  function playAnimation() {
+    const values = sceneInfo[currentScene].values;
+    const objs = sceneInfo[currentScene].objs;
+    const currentYOffset = yOffset - prevScrollHeight;
+    console.log(currentScene, currentYOffset);
+    switch (currentScene) {
+      case 0:
+        let messageA_opacity_0 = values.messageA_opacity[0];
+        let messageA_opacity_1 = values.messageA_opacity[1];
+
+        calcValues(values.messageA_opacity, currentYOffset);
+        break;
+      case 1:
+        //console.log("1 play");
+
+        break;
+      case 2:
+        //console.log("2 play");
+        break;
+      case 3:
+        // console.log("3 play");
+        break;
+    }
+  }
+
+  function calcValues(values, currentYOffset) {}
+
   function scrollLoop() {
     prevScrollHeight = 0;
     //앞의 높이 구하기
@@ -74,14 +108,16 @@
 
     if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       currentScene++;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
 
     if (yOffset < prevScrollHeight) {
       if (currentScene === 0) return;
       currentScene--;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
 
-    document.body.setAttribute("id", `show-scene-${currentScene}`);
+    playAnimation();
   }
 
   window.addEventListener("scroll", () => {
